1d52b57bab9297170cdc83d6acaa63b59fd900b5
SasinduNanayakkara <it20625016@my.sliit.lk>
2023-03-07 00:28:40 +0530

    Define routes

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const dbConnection = require('./config/db');
-
+const doctorRoutes = require('./routes/doctor.routes');
+const hospitalRoutes = require('./routes/hospital.routes');
+const nurseRoutes = require('./routes/nurse.routes');
+const loginRoutes = require('./routes/login.routes');
+const patientRoutes = require('./routes/patient.routes');
+const wardRoutes = require('./routes/ward.routes');
+const inventory = require('./routes/inventory.routes');
+const adminRoutes = require('./routes/admin.routes');
 
 const app = express();
 app.use(cors());
@@ -13,6 +20,16 @@ app.get('/', (req, res) => res.send('Hello World!'));
 
 // Connect to database
 dbConnection();
+
+// Define routes
+app.use('/api/doctor', doctorRoutes);
+app.use('/api/hospital', hospitalRoutes);
+app.use('/api/nurse', nurseRoutes);
+app.use('/api/login', loginRoutes);
+app.use('/api/patient', patientRoutes);
+app.use('/api/ward', wardRoutes);
+app.use('/api/inventory', inventory);
+app.use('/api/admin', adminRoutes);
 
 const PORT = process.env.PORT || 5000;
 
