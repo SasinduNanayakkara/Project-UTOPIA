c7acf529075d70232210179216158af37466c92f
SasinduNanayakkara <it20625016@my.sliit.lk>
2023-03-10 02:37:31 +0530

    Add new routes

--- routes/inventory.routes.js
+++ routes/inventory.routes.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
-const { registerInventory, getInventories, getInventory, inCreaseInventory, deleteInventory, decreaseInventory } = require('../controllers/inventory.controller');
+const { registerInventory, getInventories, getInventory, inCreaseInventory, deleteInventory, decreaseInventory, getInventoryByType } = require('../controllers/inventory.controller');
 
 router.post("/", registerInventory);
 router.get("/", getInventories);
 router.get("/:id", getInventory);
+router.get("/type/:type", getInventoryByType);
 router.put("/increase/:id", inCreaseInventory);
 router.put("/decrease/:id", decreaseInventory);
 router.delete("/:id", deleteInventory);
--- routes/patient.routes.js
+++ routes/patient.routes.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
-const { registerPatient, getPatients, getPatient, updatePatient, deletePatient } = require('../controllers/patient.controller');
+const { registerPatient, getPatients, getPatient, updatePatient, deletePatient, getPatientByWard } = require('../controllers/patient.controller');
 
 router.post("/", registerPatient);
 router.get("/", getPatients);
 router.get("/:id", getPatient);
+router.get("/ward/:id", getPatientByWard);
 router.put("/:id", updatePatient);
 router.delete("/:id", deletePatient);
+
 
 module.exports = router;
